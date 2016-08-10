eb9ca561988d9eb4b1a2f5740651391060b2361d
canhtranngoc <canh.tran.bm@gmail.com>
2016-08-10 12:52:22 +0700

    edit

--- TADASHBOARRD/Common/CommonActions.cs
+++ TADASHBOARRD/Common/CommonActions.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 
 namespace TADASHBOARRD.Common
 {
@@ -19,6 +23,26 @@ namespace TADASHBOARRD.Common
             Assert.AreEqual(expectedText, actualText);
         }
 
+        public void WaitForElementLoad(By locator, int timeoutInSeconds)
+        {
+            if (timeoutInSeconds > 0)
+            {
+                WebDriverWait wait = new WebDriverWait(Driver.driver, TimeSpan.FromSeconds(timeoutInSeconds));
+                wait.Until(ExpectedConditions.ElementIsVisible(locator));
+            }
+        }
+
+        public void ConfirmPopup()
+        {
+            Thread.Sleep(1000);
+            Driver.driver.SwitchTo().Alert().Accept();
+        }
+
+        public string GetTextPopup()
+        {
+            Thread.Sleep(1000);
+            return Driver.driver.SwitchTo().Alert().Text;
+        }
         public void test()
         {
             
--- TADASHBOARRD/PageActions/PanelsPage/PanelsPage.cs
+++ TADASHBOARRD/PageActions/PanelsPage/PanelsPage.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TADASHBOARRD.Common;
 
 namespace TADASHBOARRD.PageActions.PanelsPage
 {
-    class PanelsPage
+    class PanelsPage:CommonActions
     {
     }
 }
