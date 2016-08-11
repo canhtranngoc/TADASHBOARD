5b9d67c9f77641f2b765b0cdd49de9da1c5728a7
canhtranngoc <canh.tran.bm@gmail.com>
2016-08-11 14:57:09 +0700

    update "Create New Page"

--- TADASHBOARRD/PageActions/GeneralPage/NewPageDialog.cs
+++ TADASHBOARRD/PageActions/GeneralPage/NewPageDialog.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using TADASHBOARRD.Common;
+using TADASHBOARRD.PageActions;
 
 namespace TADASHBOARRD.PageActions.GeneralPage
 {
-    class NewPageDialog : CommonActions
+    public class NewPageDialog : GeneralPage
     {
+        public void CreateNewPage(string pageName)
+        {
+            Thread.Sleep(2000);
+            //string parentPage, string numberOfColumns, string displayAfter
+            EnterValue("pagename textbox", pageName);
 
+
+        }
     }
 }
