9f35e6456abe4265acbae326a9000884cc8abc3d
tranlenhatbinh <tranlenhatbinh191988@gmail.com>
2016-08-11 09:32:27 +0700

    1

--- TADASHBOARRD/Testcases/LoginTestCases.cs
+++ TADASHBOARRD/Testcases/LoginTestCases.cs
@@ -10,11 +10,12 @@ namespace TADASHBOARRD.Testcases
     public class LoginTestCases:BaseTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void DA_LOGIN_TC001_Verify_that_user_can_login_specific_repository_successfully_via_Dashboard_login_page_with_correct_credentials()
         {
             NavigateTADashboard();
             LoginPage loginPage = new LoginPage();
             loginPage.Login(TestData.defaulRepository, TestData.validUsername, TestData.validPassword);
+
         }
     }
 }
