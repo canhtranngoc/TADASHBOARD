9f48916d7b030d9349149e4f47bf0cf71d735b40
canhtranngoc <canh.tran.bm@gmail.com>
2016-08-11 12:33:56 +0700

    add TC04

--- TADASHBOARRD/Testcases/LoginTestCases.cs
+++ TADASHBOARRD/Testcases/LoginTestCases.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TADASHBOARRD.Common;
 using TADASHBOARRD.PageActions.LoginPage;
@@ -19,6 +20,22 @@ namespace TADASHBOARRD.Testcases
             GeneralPage generalPage = new GeneralPage();
             string actual= generalPage.GetUserName();
             CheckTextDisplays(actual, TestData.validUsername);
+        }
+
+        [TestMethod]
+        public void DA_LOGIN_TC004_Verify_that_user_is_able_to_log_in_different_repositories_successfully_after_logging_out_current_repository()
+        {
+            NavigateTADashboard();
+            LoginPage loginPage = new LoginPage();
+            loginPage.Login(TestData.defaulRepository, TestData.validUsername,TestData.validPassword);
+
+
+            GeneralPage generalPage = new GeneralPage();
+            generalPage.Logout();
+
+            loginPage.Login(TestData.testRepository, TestData.validUsername, TestData.validPassword);
+
+
         }
     }
 }
