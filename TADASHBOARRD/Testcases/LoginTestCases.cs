6f2be91a497ad4b804b8ae8a29e9ce5e15617904
tranlenhatbinh <tranlenhatbinh191988@gmail.com>
2016-08-11 12:49:40 +0700

    1

--- TADASHBOARRD/Testcases/LoginTestCases.cs
+++ TADASHBOARRD/Testcases/LoginTestCases.cs
@@ -23,11 +23,7 @@ namespace TADASHBOARRD.Testcases
             string actual= generalPage.GetUserName();
             CheckTextDisplays(actual, TestData.validUsername);
         }
-    }
-}
-            GeneralPage generalPage = new GeneralPage();
-            generalPage.Logout();
-        }
+
         [TestMethod]
         public void DA_LOGIN_TC006_Verify_that_Password_input_is_case_sensitive()
         {
