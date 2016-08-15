4290e9ab8d2af00b2fb5a6f96e3f477ef7480242
canhtranngoc <canh.tran.bm@gmail.com>
2016-08-15 14:52:37 +0700

    edit

--- TADASHBOARRD/PageActions/DataProfilesPage/DataProfilesPage.cs
+++ TADASHBOARRD/PageActions/DataProfilesPage/DataProfilesPage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 using TADASHBOARRD.Common;
 
 namespace TADASHBOARRD.PageActions.DataProfilesPage
@@ -14,8 +15,13 @@ namespace TADASHBOARRD.PageActions.DataProfilesPage
             Sleep(1);
             Click("add new link");
         }
-        public void DeleteProfile()
+
+        public void DeleteProfile(string name)
         {
+            string xpathLinkDelete = string.Format("//tbody//a[.='{0}']/../..//a[.='Delete']", name);
+            WebDriver.driver.FindElement(By.XPath(xpathLinkDelete)).Click();
+            AcceptAlert();
         }
+
     }
 }
