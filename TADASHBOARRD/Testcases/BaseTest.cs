86bc8e1363c970200533caa13045c43ee0c11dcc
nguyenthingocdieu <ngocdieunguyen.ece@gmail.com>
2016-08-17 16:19:48 +0700

    edit

--- TADASHBOARRD/Testcases/BaseTest.cs
+++ TADASHBOARRD/Testcases/BaseTest.cs
@@ -36,6 +36,11 @@ namespace TADASHBOARRD.Testcases
         {
             switch (TestContext.TestName)
             {
+                case "DA_MP_TC020_Verify_that_user_is_able_to_delete_sibbling_page_as_long_as_that_page_has_not_children_page_under_it":
+                    generalPage.AcceptAlert();
+                    generalPage.DeleteAllPages();
+                    generalPage.Logout();
+                    break;
                 case "DA_PANEL_TC030_Verify_that_no_special_character_is_allowed_to_be_inputted_into_Display_Name_field":
                     newPanelDialog.AcceptAlert();
                     newPanelDialog.CloseNewPanelDialog();
@@ -46,11 +51,6 @@ namespace TADASHBOARRD.Testcases
                     dataProfilesPage.OpenDataProfilesPage();
                     dataProfilesPage.DeleteAllProfiles();
                     dataProfilesPage.Logout();
-                    break;
-                case "DA_MP_TC020_Verify_that_user_is_able_to_delete_sibbling_page_as_long_as_that_page_has_not_children_page_under_it":
-                    generalPage.AcceptAlert();
-                    generalPage.DeleteAllPages();
-                    generalPage.Logout();
                     break;
                 default:
                     WebDriver.driver.Manage().Cookies.DeleteAllCookies();
