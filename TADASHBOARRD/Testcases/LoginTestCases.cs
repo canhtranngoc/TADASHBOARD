f5baa69b021b271cb6eef924c456e252da00a817
tranlenhatbinh <tranlenhatbinh191988@gmail.com>
2016-08-18 12:03:02 +0700

    1

--- TADASHBOARRD/Testcases/LoginTestCases.cs
+++ TADASHBOARRD/Testcases/LoginTestCases.cs
@@ -12,7 +12,6 @@ namespace TADASHBOARRD.Testcases
         private GeneralPage generalPage;
 
         [TestMethod]
-        [Priority(1)]
         public void DA_LOGIN_TC001_Verify_that_user_can_login_specific_repository_successfully_via_Dashboard_login_page_with_correct_credentials()
         {
             loginPage = new LoginPage();
@@ -26,7 +25,6 @@ namespace TADASHBOARRD.Testcases
         }
 
         [TestMethod]
-        [Priority(2)]
         public void DA_LOGIN_TC002_Verify_that_user_fails_to_login_specific_repository_successfully_via_Dashboard_login_page_with_incorrect_credentials()
         {
             loginPage = new LoginPage();
@@ -39,7 +37,6 @@ namespace TADASHBOARRD.Testcases
         }
 
         [TestMethod]
-        [Priority(3)]
         public void DA_LOGIN_TC004_Verify_that_user_is_able_to_login_different_repositories_successfully_after_logging_out_current_repository()
         {
             loginPage = new LoginPage();
@@ -55,7 +52,6 @@ namespace TADASHBOARRD.Testcases
         }
 
         [TestMethod]
-        [Priority(4)]
         public void DA_LOGIN_TC006_Verify_that_Password_input_is_case_sensitive()
         {
             loginPage = new LoginPage();
