58b17729d82e694c004a60ba8eca5066e32adeb6
nguyenthingocdieu <ngocdieunguyen.ece@gmail.com>
2016-08-18 09:51:56 +0700

    edit

--- TADASHBOARRD/Testcases/MainPageTestCases.cs
+++ TADASHBOARRD/Testcases/MainPageTestCases.cs
@@ -21,11 +21,11 @@ namespace TADASHBOARRD.Testcases
             generalPage = new GeneralPage();
             generalPage.OpenAddPageDialog();
             newPageDialog = new NewPageDialog();
-            string pageName1 = CommonActions.GetDateTime();
+            string pageName1 = GetDateTime();
             newPageDialog.CreateNewPage(pageName1, TestData.overviewPage, TestData.blankNumberOfColumns, TestData.blankDisplayAfter, TestData.statusPublic);
             generalPage.OpenAddPageDialog();
             newPageDialog = new NewPageDialog();
-            string pageName2 = CommonActions.GetDateTime();
+            string pageName2 = GetDateTime();
             newPageDialog.CreateNewPage(pageName2, pageName1, TestData.blankNumberOfColumns, TestData.blankDisplayAfter, TestData.statusPublic);
             generalPage.goToPage(TestData.overviewPage + "/" + pageName1);
             generalPage.PerformDelete();
