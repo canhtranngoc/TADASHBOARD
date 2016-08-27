569e5629ca49284d5a8e9277f760390899b567ce
ngantruong1801 <truongkimngan1801@gmail.com>
2016-08-27 14:09:39 +0700

    edit

--- TADASHBOARRD/Common/BrowserManager.cs
+++ TADASHBOARRD/Common/BrowserManager.cs
@@ -9,6 +9,7 @@ using OpenQA.Selenium;
 using Fenton.Selenium.SuperDriver;
 using System.Collections.Generic;
 using System.Linq;
+using TADASHBOARRD.Testcases.Test;
 
 namespace TADASHBOARRD.Common
 {
